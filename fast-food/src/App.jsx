b393bc5b605e158f76dce3fb45a537bb416d6961
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-02-01 23:39:14 +0100

    i have added the top rated items

--- fast-food/src/App.jsx
+++ fast-food/src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HeadlineCards from './components/HeadlineCards'
 import Food from './components/Food'
+import Category from './components/category'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      
       <Navbar />
       <Hero />
       <HeadlineCards />
-      <Food/>
-    
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-
+      <Food />
+      <Category/>
     
      
     </>
